651f18301405dfab7bd7335602fa7e505d3adc4f
dmlarose <dmlarose93@gmail.com>
2013-09-19 17:16:26 -0400

    Add Gamma stuff

---
+++ src/strategy/game/version/gamma/GammaStrategyGameController.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package strategy.game.version.gamma;
+
+/**
+ * The GammaStrategyGameController implements the game core for
+ * the Gamma Strategy version.
+ * @author dmlarose, ctmanuel
+ * @version Sep 23, 2013
+ */
+public class GammaStrategyGameController {
+
+}
---
+++ test/strategy/game/version/gamma/GammaStrategyMasterTest.java
@@ -0,0 +1,14 @@
+package strategy.game.version.gamma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GammaStrategyMasterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
